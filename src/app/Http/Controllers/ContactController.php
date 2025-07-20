02571e5dc35820e162507bfb67ce683c666ae059
kato hideaki <mamamacha88@gmail.com>
2025-07-20 23:41:22 +0900

    indexファイル編集

---
+++ src/app/Http/Controllers/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function confirm()
+    {
+        // ここに処理を記述していきます。
+    }
+}
